c1b0ec04ffe0c25a7322a4b41007b40988304fe8
Katrina <afakeemail@gmail.com>
2016-09-06 14:20:08 -0500

    Added toast with current time and date.

--- app/src/main/java/com/katrina/hellogit/MainActivity.java
+++ app/src/main/java/com/katrina/hellogit/MainActivity.java
@@ -2,6 +2,12 @@ package com.katrina.hellogit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("HELLO", "This is a test");
+
+        Button timeButton = (Button) findViewById(R.id.button);
+        timeButton.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String currentTime = new Date().toString();
+                Toast.makeText(MainActivity.this, "The time is " +currentTime, Toast.LENGTH_LONG).show();
+
+
+            }
+        });
     }
 }
+
